98a4bddbb712ed66ead3639a05b57d106bf88b6d
Aswin P T <aswinsureshpt@gmail.com>
2025-09-18 00:54:04 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ import { ViewCounter } from "@/components/view-counter"
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <ViewCounter />
       <Navbar />
 
       <div id="home">
