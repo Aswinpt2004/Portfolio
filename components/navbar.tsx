b2129081ed7873fcb1165adf0905c523ed3abb48
Aswin P T <aswinsureshpt@gmail.com>
2025-09-18 01:26:59 +0530

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -47,7 +47,7 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="font-bold text-xl text-primary hover:text-primary/80 transition-colors cursor-pointer">
-            Aswin P T
+            suiii
           </div>
 
           {/* Desktop Navigation */}
