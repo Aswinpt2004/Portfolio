041d8e9e48eac963db6b880b56ae31f59b792e52
Aswin P T <aswinsureshpt@gmail.com>
2025-09-18 01:03:50 +0530

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -69,7 +69,7 @@ export function HeroSection() {
               className="transition-all duration-300 transform hover:scale-105"
               asChild
             >
-              <a href="/resume/Aswin_PT_Resume.pdf" download="Aswin_PT_Resume.pdf">
+              <a href="/Aswin P T_Research.pdf" download="Aswin_PT_Resume.pdf">
                 <Download className="mr-2 h-5 w-5" />
                 Resume
               </a>
